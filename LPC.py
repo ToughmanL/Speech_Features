580bf0a8b4662fb9258d7d4603ff9478004f5bd2
ToughmanL <meiluosi@gmail.com>
2020-01-08 22:22:03 +0800

    LPC

--- LPC.py
+++ LPC.py
@@ -1,10 +1,119 @@
 #!usr/bin/python
+import matplotlib.pyplot as plt
 import numpy as np
+import wave
+import scipy.io.wavfile as wav
+from scipy import signal
+import scipy as sk
+from audiolazy import *
+from audiolazy import lpc
+from sklearn import preprocessing
+import scipy.signal as sig
+import scipy.linalg as linalg
 
-class LPC_computing:
-    def __init__(self,hammingFrames):
-        self.hammingFrames=hammingFrames
 
+def readWavFile(wav):
+    # given a path from the keyboard to read a .wav file
+    # wav = raw_input('Give me the path of the .wav file you want to read: ')
+    inputWav = '/home/gionanide/Theses_2017-2018_2519/MEEI-RainBow' + wav
+    return inputWav
+
+
+# reading the .wav file (signal file) and extract the information we need
+def initialize(inputWav):
+    rate, signal = wav.read(readWavFile(inputWav))  # returns a wave_read object , rate: sampling frequency
+    sig = wave.open(readWavFile(inputWav))
+    # signal is the numpy 2D array with the date of the .wav file
+    # len(signal) number of samples
+    sampwidth = sig.getsampwidth()
+    print('The sample rate of the audio is: ', rate)
+    print('Sampwidth: ', sampwidth)
+    return signal, rate
+
+
+# implementation of the low-pass filter
+def lowPassFilter(signal, coeff=0.97):
+    return np.append(signal[0],
+                     signal[1:] - coeff * signal[:-1])  # y[n] = x[n] - a*x[n-1] , a = 0.97 , a>0 for low-pass filters
+
+
+def preEmphasis(wav):
+    # taking the signal
+    signal, rate = initialize(wav)
+    # Pre-emphasis Stage
+    preEmphasis = 0.97
+    emphasizedSignal = lowPassFilter(signal)
+    Time = np.linspace(0, len(signal) / rate, num=len(signal))
+    EmphasizedTime = np.linspace(0, len(emphasizedSignal) / rate, num=len(emphasizedSignal))
+    # plots using matplotlib
+    '''plt.figure(figsize=(9, 7)) 
+    plt.subplot(211, facecolor='darkslategray')
+    plt.title('Signal wave')
+    plt.ylim(-50000, 50000)
+    plt.ylabel('Amplitude', fontsize=16)
+    plt.plot(Time,signal,'C1')
+    plt.subplot(212, facecolor='darkslategray')
+    plt.title('Pre-emphasis')
+    plt.ylim(-50000, 50000)
+    plt.xlabel('time(s)', fontsize=10)
+    plt.ylabel('Amplitude', fontsize=16)
+    plt.plot(EmphasizedTime,emphasizedSignal,'C1')
+    plt.show()'''
+    return emphasizedSignal, signal, rate
+
+
+def visualize(rate, signal):
+    # taking the signal's time
+    Time = np.linspace(0, len(signal) / rate, num=len(signal))
+    # plots using matplotlib
+    plt.figure(figsize=(10, 6))
+    plt.subplot(facecolor='darkslategray')
+    plt.title('Signal wave')
+    plt.ylim(-40000, 40000)
+    plt.ylabel('Amplitude', fontsize=16)
+    plt.xlabel('Time(s)', fontsize=8)
+    plt.plot(Time, signal, 'C1')
+    plt.draw()
+
+
+# plt.show()
+
+def framing(fs, signal):
+    # split the signal into frames
+    windowSize = 0.025  # 25ms
+    windowStep = 0.01  # 10ms
+    overlap = int(fs * windowStep)
+    frameSize = int(fs * windowSize)  # int() because the numpy array can take integer as an argument in the initiation
+    numberOfframes = int(np.ceil(float(np.abs(len(signal) - frameSize)) / overlap))
+    print('Overlap is: ', overlap)
+    print('Frame size is: ', frameSize)
+    print('Number of frames: ', numberOfframes)
+    frames = np.ndarray(
+        (numberOfframes, frameSize))  # initiate a 2D array with numberOfframes rows and frame size columns
+    # assing samples into the frames (framing)
+    for k in range(0, numberOfframes):
+        for i in range(0, frameSize):
+            if ((k * overlap + i) < len(signal)):
+                frames[k][i] = signal[k * overlap + i]
+            else:
+                frames[k][i] = 0
+    return frames, frameSize
+
+
+def hamming(frames, frameSize):
+    # Windowing with Hamming
+    # Hamming implementation : W[n] = 0.54 - 0.46 * numpy.cos((2 * numpy.pi * n) / (frameSize - 1))
+    # y[n] = s[n] (signal in a specific sample) * w[n] (the window function Hamming)
+    frames *= np.hamming(frameSize)
+    '''plt.figure(figsize=(10, 6)) 
+    plt.subplot(facecolor='darkslategray')
+    plt.title('Hamming window')
+    plt.ylim(-40000, 40000)
+    plt.ylabel('Amplitude', fontsize=16)
+    plt.xlabel('Time(ms)', fontsize=8)
+    plt.plot(frames,'C1')
+    plt.show()'''
+    return frames
 
 
 def autocorrelation(hammingFrames):
@@ -19,7 +128,7 @@ def autocorrelation(hammingFrames):
 def levinsonDurbin(correlateFrames):
     # normalizedCF = preprocessing.normalize(correlateFrames, norm='l2')
     filt1 = levinson_durbin(correlateFrames, 13)
-    print (filt1.numerator[1:])
+    print(filt1.numerator[1:])
 
 
 def myLPC():
@@ -27,7 +136,7 @@ def myLPC():
     amount = input('Give the number of samples in the specific folder: ')
     for x in range(1, int(amount) + 1):
         wav = '/' + folder + '/' + str(x) + '.wav'
-        print (wav)
+        print(wav)
         emphasizedSignal, signal, rate = preEmphasis(wav)
         # visualize(rate,signal)
         frames, frameSize = framing(rate, signal)
@@ -45,7 +154,7 @@ def LPC_autocorrelation(order=13):
     amount = input('Give the number of samples in the specific folder: ')
     for x in range(1, int(amount) + 1):
         wav = '/' + folder + '/' + str(x) + '.wav'
-        print (wav)
+        print(wav)
         # preemhasis filter
         emphasizedSignal, signal, rate = preEmphasis(wav)
         length = emphasizedSignal.size
@@ -56,7 +165,7 @@ def LPC_autocorrelation(order=13):
 
         # using levinson_durbin method instead of solving toeplitz
         lpc_coefficients_levinson = levinson_durbin(autocorr_coefficients, 13)
-        print ('With levinson_durbin instead of toeplitz ', lpc_coefficients_levinson.numerator)
+        print('With levinson_durbin instead of toeplitz ', lpc_coefficients_levinson.numerator)
 
         # The Toeplitz matrix has constant diagonals, with c as its first column and r as its first row. If r is not given
         R = linalg.toeplitz(autocorr_coefficients[:order])
@@ -67,7 +176,7 @@ def LPC_autocorrelation(order=13):
         lpc_features = []
         for x in lpc_coefficients:
             lpc_features.append(x)
-        print (lpc_features)
+        print(lpc_features)
 
 
 def LPC():
@@ -75,12 +184,12 @@ def LPC():
     amount = input('Give the number of samples in the specific folder: ')
     for x in range(1, int(amount) + 1):
         wav = '/' + folder + '/' + str(x) + '.wav'
-        print (wav)
+        print(wav)
         emphasizedSignal, signal, rate = preEmphasis(wav)
         filt = lpc(emphasizedSignal, order=13)
         lpc_features = filt.numerator[1:]
-        print (len(lpc_features))
-        print (lpc_features)
+        print(len(lpc_features))
+        print(lpc_features)
 
 
 def main():
